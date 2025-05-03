323b2fc10808a94c6493e86dde0246dfbf51cabc
Stephan Creutz <stephan.cr@gmx.de>
2025-05-03 18:13:02 +0200

    bump "object" version to 0.36

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ use std::ops::{Index, IndexMut};
 use std::path::PathBuf;
 
 use clap::{crate_name, crate_version, value_parser, Arg, Command};
-use object::{Object, ObjectSection, ObjectSegment, SectionKind};
 use object::{Architecture, BinaryFormat, Object, ObjectSection, ObjectSegment, SectionKind};
 
 #[derive(Debug)]
